1ab478099bc8969bef76720ca4390dd03a21932c
Truc Lam Nguyen <trucnguyenlam@gmail.com>
2025-10-16 00:21:24 +0100

    Fix script

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,6 +10,10 @@ export ICU_BUILD_HASH=588e431f77327c39031ffbb8843c0e3bc122c211374485fa87dc5f3faf
 # for page size alignment
 export ANDROID_NDK=~/sdks/android/ndk/28.2.13676358
 
+rm -rf build-android-arm64-v8a
+cmake -B build-android-arm64-v8a -DCMAKE_BUILD_TYPE=Release -DBUILD_ICU=ON -DICU_BUILD_VERSION=$ICU_BUILD_VERSION -DICU_BUILD_HASH=$ICU_BUILD_HASH -DANDROID_PLATFORM=$ANDROID_PLATFORM -DANDROID_ABI=arm64-v8a -DANDROID_TOOLCHAIN=clang -DANDROID_NDK=$ANDROID_NDK -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake
+cmake --build build-android-arm64-v8a --config Release
+
 rm -rf build-android-armeabi-v7a
 cmake -B build-android-armeabi-v7a -DCMAKE_BUILD_TYPE=Release -DBUILD_ICU=ON -DICU_BUILD_VERSION=$ICU_BUILD_VERSION -DICU_BUILD_HASH=$ICU_BUILD_HASH -DANDROID_PLATFORM=$ANDROID_PLATFORM -DANDROID_ABI=armeabi-v7a -DANDROID_TOOLCHAIN=clang -DANDROID_NDK=$ANDROID_NDK -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake
 cmake --build build-android-armeabi-v7a --config Release
@@ -18,15 +22,11 @@ rm -rf build-android-i686
 cmake -B build-android-i686 -DCMAKE_BUILD_TYPE=Release -DBUILD_ICU=ON -DICU_BUILD_VERSION=$ICU_BUILD_VERSION -DICU_BUILD_HASH=$ICU_BUILD_HASH -DANDROID_PLATFORM=$ANDROID_PLATFORM -DANDROID_ABI=x86 -DANDROID_TOOLCHAIN=clang -DANDROID_NDK=$ANDROID_NDK -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake
 cmake --build build-android-i686 --config Release
 
-rm -rf build-android-arm64-v8a
-cmake -B build-android-arm64-v8a -DCMAKE_BUILD_TYPE=Release -DBUILD_ICU=ON -DICU_BUILD_VERSION=$ICU_BUILD_VERSION -DICU_BUILD_HASH=$ICU_BUILD_HASH -DANDROID_PLATFORM=$ANDROID_PLATFORM -DANDROID_ABI=arm64-v8a -DANDROID_TOOLCHAIN=clang -DANDROID_NDK=$ANDROID_NDK -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake
-cmake --build build-android-arm64-v8a --config Release
-
 rm -rf build-android-x86_64
 cmake -B build-android-x86_64 -DCMAKE_BUILD_TYPE=Release -DBUILD_ICU=ON -DICU_BUILD_VERSION=$ICU_BUILD_VERSION -DICU_BUILD_HASH=$ICU_BUILD_HASH -DANDROID_PLATFORM=$ANDROID_PLATFORM -DANDROID_ABI=x86_64 -DANDROID_TOOLCHAIN=clang -DANDROID_NDK=$ANDROID_NDK -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake
 cmake --build build-android-x86_64 --config Release
 
-copy to ndk
+# copy to ndk
 cp -a build-android-x86_64/icu_cross/include/.  ${ANDROID_NDK}/toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include/
 
 cp -a build-android-armeabi-v7a/icu_cross/lib/. ${ANDROID_NDK}/toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/lib/arm-linux-androideabi/
